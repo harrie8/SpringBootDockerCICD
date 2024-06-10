33b63f4a07a9e4bfca62f3413014d71bbfcb1ba9
harrie8 <hesjs5@naver.com>
2024-06-10 16:24:20 +0900

    Create CookieTestController.java

---
+++ src/main/java/com/sppart/admin/sub/user/controller/CookieTestController.java
@@ -0,0 +1,25 @@
+package com.sppart.admin.sub.user.controller;
+
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CookieTestController {
+
+    @GetMapping("/cookie/test")
+    public String cookieTest(HttpServletResponse response) {
+        Cookie cookie = new Cookie("myCookie", "myCookieValue");
+
+        cookie.setPath("/");
+        cookie.setHttpOnly(true);
+        cookie.setSecure(true);
+        cookie.setMaxAge(7 * 24 * 60 * 60);
+
+        // SameSite=None 설정 추가
+        response.addHeader("Set-Cookie", String.format("%s; SameSite=None; Secure", cookie.toString()));
+
+        return "Cookie is set";
+    }
+}
